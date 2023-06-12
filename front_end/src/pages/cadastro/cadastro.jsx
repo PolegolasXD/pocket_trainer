2c883ed0c54f99738239e4e583ceb68ccf11839b
PolegolasXD <polegolasplays@gmail.com>
2023-06-12 00:16:37 -0300

    fix

--- front_end/src/pages/cadastro/cadastro.jsx
+++ front_end/src/pages/cadastro/cadastro.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import styles from './styles.module.css';
 import iconCadastro from '../../assets/icons/IconesCadastro.png';
 import iconUser from '../../assets/icons/IconesUsuario.png';
-
+ 
 function CadastroHTML() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
